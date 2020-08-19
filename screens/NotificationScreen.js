79ff277571cea886bf416663ebc7f314707ffe08
MohnishShridhar <56827136+MohnishShridhar@users.noreply.github.com>
2020-08-19 20:09:33 +0530

    Add files via upload

---
+++ screens/NotificationScreen.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView, TouchableOpacity, ScrollView, FlatList } from 'react-native';
+import db from '../config';
+import firebase from 'firebase';
+import MyHeader from '../components/MyHeader'
+import { Card, Header, Icon, ListItem } from 'react-native-elements'
+
+export default class NotificationScreen extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            userId:firebase.auth().currentUser.email,
+            allNotifications:[]
+        }
+        this.notificationRef=null
+    }
+
+    getNotifications=()=>{
+        this.notificationRef=db.collection('all_notifications').where('notificiation_status', '==', "unread")
+        .where("targeted_user_id", '==', this.state.userId)
+        .onSnapshot((snapshot) =>{
+           var allNotifications=[]
+           snapshot.docs.map((doc)=>{
+               var notification=doc.data();
+               notification["doc_id"]=doc.id
+               allNotifications.push(notification)
+           });
+           this.setState({
+               allNotifications:allNotifications
+           })
+        })
+    }
+
+    keyExtractor=(item, index)=>index.toString()
+
+    renderItem=({item, i})=>(
+        <ListItem
+        key={i}
+        title={item.book_name}
+        leftElement={<Icon name="book" type="font-awesome" color="#696969"/>}
+        titleStyle={{color:'black', fontWeight:'bold'}}
+        subtitle={item.message}
+        bottomDivider
+        />
+    )
+
+    componentDidMount(){
+        this.getNotifications()
+    }
+    componentWillUnmount(){
+        this.notificationRef()
+    }
+
+    render(){
+        return(
+            <View style={{flex:1}}>
+                <View style={{flex:0.1}}>
+                    <MyHeader title={"Notification"} navigation={this.props.navigation}/>
+                </View>
+            
+                <View style={{flex:0.9}}>
+                        {
+                            this.state.allNotifications.length===0
+                            ?(
+                                <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                                    <Text style={{fontSize:20}}>You have no notifications</Text>
+                                </View>
+                            ):(
+                                <FlatList
+                                keyExtractor={this.keyExtractor}
+                                data={this.state.allNotifictions}
+                                renderItem={this.renderItem}
+                                />
+                            )
+                        }
+                </View>
+            </View>
+        )
+    }
+}
+
